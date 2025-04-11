5beec121ef599dcb608e41fdf48ae3c63d9767f6
GuilhermeMarques1 <g-marques@outlook.com>
2025-04-11 00:31:49 -0300

    feat: add Sidebar component and styling, restructure App component layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
+
 import './global.css'
+
+import styles from './App.module.css'
 
 function App() {
   return (
-    <>
+    <div>
       <Header />
-      <h1>Hello world</h1>
-    </>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <h1>Hello world</h1>
+        </main>
+      </div>
+    </div>
   )
 }
 
