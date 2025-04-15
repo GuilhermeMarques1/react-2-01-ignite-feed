eead516d188f91ec7489be01ca696b3399949e33
GuilhermeMarques1 <g-marques@outlook.com>
2025-04-15 00:24:24 -0300

    feat: Add html structure for comments

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/guilhermemarques1.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Guilherme Marques</strong>
+              <time title="15 de abril de 2025" datetime="2025-04-15 00:07:35">Cerca de 1h atrás</time>
+            </div>
+
+            {/* Para button que é apenas um ícone ou que não tem texto dentro dele, para acessibilidade é bom ter um title */}
+            <button title="Deleter comentário">
+              <Trash size={20}/>
+            </button>
+          </header>
+
+          <p>Muito bom dev, parabéns !!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
