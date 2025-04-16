f22518ef69e78e950bb74b67898988e783dfd64a
GuilhermeMarques1 <g-marques@outlook.com>
2025-04-15 23:20:36 -0300

    feat: Create Avatar component and integrate it into Post, Comment, and Sidebar components

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import { PencilLine } from 'phosphor-react'
 
 import styles from './Sidebar.module.css'
@@ -8,7 +9,7 @@ export function Sidebar() {
       <img className={styles.cover} src="https://images.unsplash.com/photo-1605379399642-870262d3d051?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/guilhermemarques1.png" alt="" />
+        <Avatar src="https://github.com/guilhermemarques1.png"/>
 
         <strong>Guilherme Marques</strong>
         <span>Web Developer</span>
