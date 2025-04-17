5e584c5fca3e68b19ddd539c2fc9847ef4e4c5b6
GuilhermeMarques1 <g-marques@outlook.com>
2025-04-17 01:22:56 -0300

    feat: Add date-fns for date formatting and update Post component to display dynamic content

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,51 @@ import './global.css'
 import styles from './App.module.css'
 import { Post } from './components/Post'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Desenvolvedor Web"
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋' },
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2025-04-16 22:13:34')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @ Rocketseat"
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋' },
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2025-03-12 07:13:34')
+  },
+  {
+    id: 3,
+    author: {
+      avatarUrl: "https://github.com/guilhermemarques1.png",
+      name: "Guilherme Marques",
+      role: "Desenvolvedor Web"
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋' },
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2025-02-03 20:13:34')
+  },
+]
+
 function App() {
   return (
     <div>
@@ -15,7 +60,15 @@ function App() {
         <Sidebar />
 
         <main>
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
