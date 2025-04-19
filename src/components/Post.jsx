18c0af0b670e423eabf146a58fdf8ef65ec8228b
GuilhermeMarques1 <g-marques@outlook.com>
2025-04-19 16:47:42 -0300

    feat: Update Comment and Post components to handle dynamic comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -8,7 +8,10 @@ import { useState } from 'react'
 
 
 export function Post({ author, publishedAt, content }) {
-  const [comments, setComments] = useState([1, 2, 3])
+  const [comments, setComments] = useState([
+    'Post muito bacana, hein?!'
+  ])
+  const [newCommentText, setNewCommentText] = useState('')
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR
@@ -22,12 +25,18 @@ export function Post({ author, publishedAt, content }) {
   function handleCreateNewComment() {
     event.preventDefault()
 
-    setComments([...comments, comments.length + 1])
+    setComments([...comments, newCommentText])
+
+    setNewCommentText('')
+  }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value)
   }
 
   return (
     <article className={styles.post}>
-      <header>
+      <header> 
         <div className={styles.author}>
           <Avatar src={author.avatarUrl}/>
           <div className={styles.authorInfo}>
@@ -55,7 +64,10 @@ export function Post({ author, publishedAt, content }) {
         <strong>Deixe seu feedback</strong>
 
         <textarea 
+          name="comment"
           placeholder="Deixe um comentário"          
+          value={newCommentText}
+          onChange={handleNewCommentChange}
         />
 
         <footer>
@@ -67,7 +79,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment />
+          return <Comment content={comment} />
         })}
       </div>
     </article>
